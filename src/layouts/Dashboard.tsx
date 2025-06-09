f1fb31b6a1de7a2e864823e86b314c1cfa30a57c
zahidhasanzh <zahidhasanzh000@gmail.com>
2025-06-09 20:19:09 +0600

    add protected routes

--- src/layouts/Dashboard.tsx
+++ src/layouts/Dashboard.tsx
@@ -1,11 +1,17 @@
-import { Outlet } from "react-router-dom";
-
+import {Navigate, Outlet } from "react-router-dom";
+import { useAuthStore } from "../store";
 
 const Dashboard = () => {
+  const { user } = useAuthStore();
+
+  if (user === null) {
+    return <Navigate to="/auth/login" replace={true} />;
+  }
+
   return (
     <div>
-        <h1>Dashboard component</h1>
-        <Outlet/>
+      <h1>Dashboard component</h1>
+      <Outlet />
     </div>
   );
 };
--- src/layouts/NonAuth.tsx
+++ src/layouts/NonAuth.tsx
@@ -1,10 +1,16 @@
-import { Outlet } from "react-router-dom";
+import { Navigate, Outlet } from "react-router-dom";
+import { useAuthStore } from "../store";
 
 const NonAuth = () => {
+  const { user } = useAuthStore();
+
+  if (user !== null) {
+    return <Navigate to="/" replace={true} />;
+  }
   return (
-     <div>
-        <h1>NoAuth component</h1>
-        <Outlet/>
+    <div>
+      <h1>NoAuth component</h1>
+      <Outlet />
     </div>
   );
 };
