ce60b02bb597df50f3857b99fbb752d86bcc599c
zahidhasanzh <zahidhasanzh000@gmail.com>
2025-06-16 20:17:21 +0600

    add intended url redirect

--- src/layouts/Dashboard.tsx
+++ src/layouts/Dashboard.tsx
@@ -1,4 +1,4 @@
-import { Navigate, NavLink, Outlet } from "react-router-dom";
+import { Navigate, NavLink, Outlet, useLocation } from "react-router-dom";
 import Icon, { BellFilled } from "@ant-design/icons";
 import { useAuthStore } from "../store";
 import {
@@ -61,6 +61,7 @@ const getMenuItems = (role: string) => {
 };
 
 const Dashboard = () => {
+  const location = useLocation()
   const { logout: logoutFromStore } = useAuthStore();
 
   const { mutate: logoutMutate } = useMutation({
@@ -81,7 +82,7 @@ const Dashboard = () => {
   const { user } = useAuthStore();
 
   if (user === null) {
-    return <Navigate to="/auth/login" replace={true} />;
+    return <Navigate to={`/auth/login?returnTo=${location.pathname}`} replace={true} />;
   }
 
   const items = getMenuItems(user.role);
--- src/layouts/NonAuth.tsx
+++ src/layouts/NonAuth.tsx
@@ -1,10 +1,12 @@
-import { Navigate, Outlet } from "react-router-dom";
+import { Navigate, Outlet, useLocation } from "react-router-dom";
 import { useAuthStore } from "../store";
 
 const NonAuth = () => {
+  const location = useLocation()
   const { user } = useAuthStore();
   if (user !== null) {
-    return <Navigate to="/" replace={true} />;
+    const returnTo = new URLSearchParams(location.search).get('returnTo') || '/'
+    return <Navigate to={returnTo} replace={true} />;
   }
   return (
     <div>
