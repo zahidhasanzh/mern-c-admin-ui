e3bfa705abf83719a76f4a99e6a5924f4ac6620f
zahidhasanzh <zahidhasanzh000@gmail.com>
2025-06-16 19:54:59 +0600

    reset pagination on filter change

--- src/pages/users/Users.tsx
+++ src/pages/users/Users.tsx
@@ -132,7 +132,7 @@ const Users = () => {
 
   const debouncedQUpdate = useMemo(() => {
      return debounce((value: string | undefined) => {
-      setQueryParams((prev) => ({...prev, q: value}))
+      setQueryParams((prev) => ({...prev, q: value, currentPage: 1}))
      }, 500)
   }, [])
 
@@ -147,7 +147,7 @@ const Users = () => {
     if ("q" in changeFilterFields) {
       debouncedQUpdate(changeFilterFields.q)
     } else {
-      setQueryParams((prev) => ({ ...prev, ...changeFilterFields }));
+      setQueryParams((prev) => ({ ...prev, ...changeFilterFields, currentPage: 1 }));
     }
   };
 
