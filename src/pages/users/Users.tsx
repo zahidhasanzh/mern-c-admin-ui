6ebc4eb2c3c45874150f98a1a44d18c9ac6eb654
zahidhasanzh <zahidhasanzh000@gmail.com>
2025-06-16 19:49:20 +0600

    change debounche timing

--- src/pages/users/Users.tsx
+++ src/pages/users/Users.tsx
@@ -133,7 +133,7 @@ const Users = () => {
   const debouncedQUpdate = useMemo(() => {
      return debounce((value: string | undefined) => {
       setQueryParams((prev) => ({...prev, q: value}))
-     }, 1000)
+     }, 500)
   }, [])
 
   const onFilterChange = (changedFields: FieldData[]) => {
